1ac6746185c35c39d3cee6c72e7a3d2b0863062e
Artem Savchenko <artemsavcenko963@gmail.com>
2025-08-31 12:44:42 +0200

    Add main.js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("testButton").addEventListener("click", function () {
+    alert("Button clicked! 🎉");
+});
+
+// document.getElementById("myTestContainer").addEventListener("mouseover", function () {
+//     alert("Something happened! 🎉");
+// });
