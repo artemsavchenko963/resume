6cf66e483ba2d3b792710d2b498e2c7ca3dfbc24
Artem Savchenko <artemsavcenko963@gmail.com>
2025-08-31 21:53:25 +0200

    Create a new JS file and add two buttons to Details.html

---
+++ js/table.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("button")
+const tableContainer = document.getElementById("tableContainer")
+
+button.addEventListener("click", () => {
+    if (tableContainer.style.display === "none") {
+        tableContainer.style.display = "block";
+        button.textContent = "Hide table";
+    } else {
+        tableContainer.style.display = "none";
+        button.textContent = "Show table";
+    }
+});
+
+
+const button1 = document.getElementById('button1');
+const tooltrip12 = document.getElementById('list');
+
+button1.addEventListener("mouseenter", () => {
+    tooltrip12.style.display = "block";
+    button1.textContent = "Hover!";
+    button1.style.color = "blue";
+});
+
+button1.addEventListener("mouseleave", () => {
+    tooltrip12.style.display = "none";
+    button1.textContent = "Hover!";
+    button1.style.color = "black"
+});
